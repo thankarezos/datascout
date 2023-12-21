0fa11ca9dfe2ce11fbcca6f40e71254cd62ac388
Thanasis Karezos <thankarezos@gmail.com>
2023-12-21 21:10:30 +0200

    init

---
+++ backend/src/main/kotlin/com/datascout/datascout/DatascoutApplication.kt
@@ -0,0 +1,11 @@
+package com.datascout.datascout
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DatascoutApplication
+
+fun main(args: Array<String>) {
+	runApplication<DatascoutApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/datascout/datascout/DatascoutApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.datascout.datascout
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DatascoutApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
