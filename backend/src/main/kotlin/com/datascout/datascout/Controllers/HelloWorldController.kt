34fd8a18f01a3d4b01ebdbad1845f0e57362d94d
Thanasis Karezos <thankarezos@gmail.com>
2023-12-21 21:56:27 +0200

    test controller

---
+++ backend/src/main/kotlin/com/datascout/datascout/Controllers/HelloWorldController.kt
@@ -0,0 +1,13 @@
+package com.datascout.datascout.Controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/helloWorld")
+    fun sayHello(): String {
+        return "Hello World!"
+    }
+}
