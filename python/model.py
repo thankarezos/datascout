1bc987ef40f7611fa79a5a6f2c10dc93e1c53d5d
EnaAlogo <93442086+EnaAlogo@users.noreply.github.com>
2024-01-14 20:59:36 +0200

    Add files via upload
    Add files via upload

--- python/model.py
+++ python/model.py
@@ -79,11 +79,12 @@ def test():
    
    image.show()
 
+
 class Request(BaseModel):
-    m_Bytes : bytes | str = None
-    m_Link : str = None
-    m_Whitelist : list[str] = []
-    m_DoAnnotate : bool = False
+    m_Bytes : Optional[bytes] | Optional[str] = None
+    m_Link : Optional[str] = None
+    m_Whitelist : Optional[list[str]] = []
+    m_DoAnnotate : Optional[bool] = False
 
 
 @app.get("/infer")
